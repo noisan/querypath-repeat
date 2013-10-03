6dd34550768f2e26ab309d6f490cb74fe30f7e74
Akihiro Yamanoi <akihiro.yamanoi@gmail.com>
2013-10-03 21:25:08 +0900

    Add integration tests for `repeatInner()`

--- tests/integration/RepeatExtensionIntegrationTest.php
+++ tests/integration/RepeatExtensionIntegrationTest.php
@@ -27,6 +27,18 @@ class RepeatExtensionIntegrationTest extends \PHPUnit_Framework_TestCase
         $this->assertDomEqualsXmlString($testXML, $qp);
     }
 
+    public function testRepeatInner_EmptyQuery_RepeatsNothing()
+    {
+        // Setup
+        $testXML = '<?xml version="1.0"?><root />';
+
+        // Act
+        $qp = qp($testXML)->find('item')->repeatInner(5);
+
+        // Assert
+        $this->assertDomEqualsXmlString($testXML, $qp);
+    }
+
     public function testRepeat_ZeroTimes_RemovesSelectedNode()
     {
         // Setup
@@ -35,6 +47,19 @@ class RepeatExtensionIntegrationTest extends \PHPUnit_Framework_TestCase
 
         // Act
         $qp = qp($testXML)->find('item')->repeat(0);
+
+        // Assert
+        $this->assertDomEqualsXmlString($expected, $qp);
+    }
+
+    public function testRepeatInner_ZeroTimes_RemovesChildrenOfSelectedNode()
+    {
+        // Setup
+        $testXML = '<?xml version="1.0"?><root><item>TEST</item></root>';
+        $expected = '<?xml version="1.0"?><root><item></item></root>';
+
+        // Act
+        $qp = qp($testXML)->find('item')->repeatInner(0);
 
         // Assert
         $this->assertDomEqualsXmlString($expected, $qp);
@@ -49,6 +74,20 @@ class RepeatExtensionIntegrationTest extends \PHPUnit_Framework_TestCase
 
         // Act
         $qp = qp($testXML)->find('item')->repeat($counter);
+
+        // Assert
+        $this->assertDomEqualsXmlString($expected, $qp);
+    }
+
+    public function testRepeatInner_SingleNode_RepeatsChildrenOfTargetNode()
+    {
+        // Setup
+        $counter = 2;
+        $testXML = '<?xml version="1.0"?><root><item><b>Child A</b><i>Child B</i></item></root>';
+        $expected = '<?xml version="1.0"?><root><item><b>Child A</b><i>Child B</i><b>Child A</b><i>Child B</i></item></root>';
+
+        // Act
+        $qp = qp($testXML)->find('item')->repeatInner($counter);
 
         // Assert
         $this->assertDomEqualsXmlString($expected, $qp);
@@ -69,6 +108,26 @@ class RepeatExtensionIntegrationTest extends \PHPUnit_Framework_TestCase
 
         // Act
         $qp = qp($testXML)->find('item')->repeat($counter);
+
+        // Assert
+        $this->assertDomEqualsXmlString($expected, $qp);
+    }
+
+    public function testRepeatInner_MultipleNodes_RepeatsChildrenOfEachSelectedNodes()
+    {
+        // Setup
+        $counter = 3;
+        $testXML = '<?xml version="1.0"?><root>' .
+                '<div><item><i>A1</i><p>A2</p></item></div>' .
+                '<div><item><i>B1</i><p>B2</p></item></div>' .
+                '</root>';
+        $expected = '<?xml version="1.0"?><root>' .
+                '<div><item><i>A1</i><p>A2</p><i>A1</i><p>A2</p><i>A1</i><p>A2</p></item></div>' .
+                '<div><item><i>B1</i><p>B2</p><i>B1</i><p>B2</p><i>B1</i><p>B2</p></item></div>' .
+                '</root>';
+
+        // Act
+        $qp = qp($testXML)->find('item')->repeatInner($counter);
 
         // Assert
         $this->assertDomEqualsXmlString($expected, $qp);
@@ -98,6 +157,30 @@ class RepeatExtensionIntegrationTest extends \PHPUnit_Framework_TestCase
         $this->assertDomEqualsXmlString($expected, $qp);
     }
 
+    public function testRepeatInner_RemovedNode_RepeatsChildrenOfRemovedNode()
+    {
+        // Setup
+        $counter = 2;
+        $testXML = '<?xml version="1.0"?><root>' .
+                '<from><item><p>TEST</p></item></from>'.
+                '<to />' .
+                '</root>';
+        $expected = '<?xml version="1.0"?><root>' .
+                '<from />'.
+                '<to><item><p>TEST</p><p>TEST</p></item></to>' .
+                '</root>';
+
+        $qp = qp($testXML);
+        $dest = $qp->find('to');
+
+        // Act
+        $removed = $qp->remove('item');
+        $removed->repeatInner($counter)->appendTo($dest);
+
+        // Assert
+        $this->assertDomEqualsXmlString($expected, $qp);
+    }
+
     public function testRepeat_CallbackGiven_InvokesCallbackOnEachRepeat()
     {
         // Setup
@@ -116,6 +199,25 @@ class RepeatExtensionIntegrationTest extends \PHPUnit_Framework_TestCase
         $this->assertDomEqualsXmlString($expected, $qp);
     }
 
+    public function testRepeatInner_CallbackGiven_InvokesCallbackOnEachRepeat()
+    {
+        // Setup
+        $callback = function ($i, $node) {
+            $p = qp($node)->find('p');
+            $p->text(strtoupper($p->text() . ':' . $i));
+        };
+
+        $counter = 2;
+        $testXML = '<?xml version="1.0"?><root><item><p>test</p></item></root>';
+        $expected = '<?xml version="1.0"?><root><item><p>TEST:0</p><p>TEST:1</p></item></root>';
+
+        // Act
+        $qp = qp($testXML)->find('item')->repeatInner($counter, $callback);
+
+        // Assert
+        $this->assertDomEqualsXmlString($expected, $qp);
+    }
+
     public function testRepeat_ArrayCounterGiven_InvokesCallbackWithEachArrayElement()
     {
         // Setup
@@ -129,6 +231,24 @@ class RepeatExtensionIntegrationTest extends \PHPUnit_Framework_TestCase
 
         // Act
         $qp = qp($testXML)->find('item')->repeat($counter, $callback);
+
+        // Assert
+        $this->assertDomEqualsXmlString($expected, $qp);
+    }
+
+    public function testRepeatInner_ArrayCounterGiven_InvokesCallbackWithEachArrayElement()
+    {
+        // Setup
+        $counter = array('A', 'B', 'C');
+
+        $callback = function ($i, $node) {
+            $node->nodeValue = $i;
+        };
+        $testXML = '<?xml version="1.0"?><root><item>Test</item></root>';
+        $expected = '<?xml version="1.0"?><root><item>ABC</item></root>';
+
+        // Act
+        $qp = qp($testXML)->find('item')->repeatInner($counter, $callback);
 
         // Assert
         $this->assertDomEqualsXmlString($expected, $qp);
@@ -154,6 +274,26 @@ class RepeatExtensionIntegrationTest extends \PHPUnit_Framework_TestCase
         $this->assertDomEqualsXmlString($expected, $qp);
     }
 
+    public function testRepeatInner_TraversableCounterGiven_InvokesCallbackWithEachIteratedValue()
+    {
+        // Setup
+        $counter = new \RegexIterator(new \ArrayIterator(array(
+            'Apple', 'Banana', 'Kiwi', 'Lemon', 'Orange', 'Peach',
+        )), '/e$/');
+
+        $callback = function ($fruit, $node) {
+            qp($node)->find('p')->text($fruit);
+        };
+        $testXML = '<?xml version="1.0"?><root><item><p>Test</p></item></root>';
+        $expected = '<?xml version="1.0"?><root><item><p>Apple</p><p>Orange</p></item></root>';
+
+        // Act
+        $qp = qp($testXML)->find('item')->repeatInner($counter, $callback);
+
+        // Assert
+        $this->assertDomEqualsXmlString($expected, $qp);
+    }
+
     public function testRepeat_CallbackReturningFalse_BreaksRepeat()
     {
         // Setup
@@ -174,6 +314,26 @@ class RepeatExtensionIntegrationTest extends \PHPUnit_Framework_TestCase
         $this->assertDomEqualsXmlString($expected, $qp);
     }
 
+    public function testRepeatInner_CallbackReturningFalse_BreaksRepeat()
+    {
+        // Setup
+        $counter = 10;
+        $callback = function ($i, $unused) {
+            if (3 <= $i) {
+                return false;
+            }
+        };
+
+        $testXML = '<?xml version="1.0"?><root><item>Test</item></root>';
+        $expected = '<?xml version="1.0"?><root><item>TestTestTest</item></root>';
+
+        // Act
+        $qp = qp($testXML)->find('item')->repeatInner($counter, $callback);
+
+        // Assert
+        $this->assertDomEqualsXmlString($expected, $qp);
+    }
+
     public function testRepeat_ReturnsQueryPathInstance()
     {
         // Setup
@@ -183,6 +343,22 @@ class RepeatExtensionIntegrationTest extends \PHPUnit_Framework_TestCase
 
         // Act
         $result = $qp->repeat(3);
+        $result->textAfter('OK');
+
+        // Assert
+        $this->assertInstanceOf(get_class($qp), $result);
+        $this->assertDomEqualsXmlString($expected, $qp);
+    }
+
+    public function testRepeatInner_ReturnsQueryPathInstance()
+    {
+        // Setup
+        $testXML = '<?xml version="1.0"?><root><item><p>A</p></item><item><p>B</p></item></root>';
+        $expected = '<?xml version="1.0"?><root><item><p>A</p><p>A</p></item>OK<item><p>B</p><p>B</p></item>OK</root>';
+        $qp = qp($testXML)->find('item');
+
+        // Act
+        $result = $qp->repeatInner(2);
         $result->textAfter('OK');
 
         // Assert
