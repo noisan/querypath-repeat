9ed243e1c43553c0bc7516520c444f4a83026a09
Akihiro Yamanoi <akihiro.yamanoi@gmail.com>
2013-10-05 19:16:16 +0900

    Add doc comments

--- lib/Noi/QueryPath/RepeatExtension.php
+++ lib/Noi/QueryPath/RepeatExtension.php
@@ -7,6 +7,46 @@ use QueryPath\Exception;
 use SplObjectStorage;
 
 /**
+ * A QueryPath extension that adds extra methods for repeating selected elements.
+ *
+ * This extension provides two methods:
+ *
+ * - repeat()
+ * - repeatInner()
+ *
+ * Usage:
+ * <code>
+ * <?php
+ * QueryPath::enable('Noi\QueryPath\RepeatExtension');
+ * $qp = qp('<?xml version="1.0"?><root><div><p>Test</p></div></root>');
+ *
+ * $qp->find('div')->repeat(2);
+ * $qp->find('div')->repeatInner(array('Apple', 'Orange', 'Lemon'), function ($name, $node) {
+ *     qp($node, 'p')->text($name);
+ * });
+ *
+ * $qp->writeXML();
+ * </code>
+ *
+ * OUTPUT:
+ * <code>
+ * <?xml version="1.0"?>
+ * <root>
+ *   <div>
+ *     <p>Apple</p>
+ *     <p>Orange</p>
+ *     <p>Lemon</p>
+ *   </div>
+ *   <div>
+ *     <p>Apple</p>
+ *     <p>Orange</p>
+ *     <p>Lemon</p>
+ *   </div>
+ * </root>
+ * </code>
+ *
+ * @see RepeatExtension::repeat()
+ * @see RepeatExtension::repeatInner()
  *
  * @author Akihiro Yamanoi <akihiro.yamanoi@gmail.com>
  */
@@ -19,6 +59,38 @@ class RepeatExtension implements Extension
         $this->qp = $qp;
     }
 
+    /**
+     * Repeats each element in the current DOMQuery object.
+     *
+     * Usage:
+     * <code>
+     * <?php
+     * QueryPath::enable('Noi\QueryPath\RepeatExtension');
+     * $qp = qp('<?xml version="1.0"?><root><div>Test</div></root>');
+     *
+     * $qp->find('div')->repeat(3, function ($i, $node) {
+     *     qp($node)->text('Repeat: ' . $i);
+     * });
+     *
+     * $qp->writeXML();
+     * </code>
+     *
+     * OUTPUT:
+     * <code>
+     * <?xml version="1.0"?>
+     * <root>
+     *   <div>Repeat: 0</div>
+     *   <div>Repeat: 1</div>
+     *   <div>Repeat: 2</div>
+     * </root>
+     * </code>
+     *
+     * @param integer|array|\Traversable $counter
+     * @param callable|null $callback An optional callback that will be called
+     *                                with each value of counter as the first argument
+     *                                and each cloned node as the second argument
+     * @return \QueryPath\DOMQuery The DOMQuery object, with the repeated element(s) selected
+     */
     public function repeat($counter, $callback = null)
     {
         $repeated = new SplObjectStorage();
@@ -34,6 +106,44 @@ class RepeatExtension implements Extension
         return $this->qp;
     }
 
+    /**
+     * Repeats the child (inner) nodes of each element in the current DOMQuery object.
+     *
+     * Usage:
+     * <code>
+     * <?php
+     * QueryPath::enable('Noi\QueryPath\RepeatExtension');
+     * $qp = qp('<?xml version="1.0"?><root><div><b>Test</b><i>Test</i></div></root>');
+     *
+     * $qp->find('div')->repeatInner(3, function ($i, $node) {
+     *     qp($node, 'b')->text('Repeat: ' . $i);
+     *     qp($node, 'i')->text('Repeat: ' . $i);
+     * });
+     *
+     * $qp->writeXML();
+     * </code>
+     *
+     * OUTPUT:
+     * <code>
+     * <?xml version="1.0"?>
+     * <root>
+     *   <div>
+     *     <b>Repeat: 0</b>
+     *     <i>Repeat: 0</i>
+     *     <b>Repeat: 1</b>
+     *     <i>Repeat: 1</i>
+     *     <b>Repeat: 2</b>
+     *     <i>Repeat: 2</i>
+     *   </div>
+     * </root>
+     * </code>
+     *
+     * @param integer|array|\Traversable $counter
+     * @param callable|null $callback  An optional callback that will be called
+     *                                 with each value of counter as the first argument
+     *                                 and each element
+     * @return \QueryPath\DOMQuery The DOMQuery object with the same element(s) selected
+     */
     public function repeatInner($counter, $callback = null)
     {
         foreach ($this->qp->get() as $templateNode) {
